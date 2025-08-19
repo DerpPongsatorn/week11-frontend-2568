fe88865047cd5a0d919756931cf1d9a0833c1ca9
DerpPongsatorn <trstiphone@gmail.com>
2025-08-19 12:04:25 +0700

    update

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { NuxtLayout } from '#components';
+
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage-/>
+  </NuxtLayout>
+  
+
+</template>
---
+++ app/components/app/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="font-[sant-serif] bg-gray-100 min-h-screen">
+    <!-- start <Header -->
+<div class="text-gray-600 bg-[#5B1282] ">
+  <Nav class="container px-6 py-[0.8] mx-auto md:flex md:justify-between md:items-center shadow-xl- pl-5 md:px-30 z-0 cursor-pointer">
+  <h1>header</h1>
+  </Nav>
+</div>
+    <!-- End <Header -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
