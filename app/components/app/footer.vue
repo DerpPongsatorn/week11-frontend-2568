541ca71ea05b58a19c2cffba4fdb33097fec62a3
Pongstorm Potita <gaymanxii1995@gmail.com>
2025-09-02 10:56:01 +0700

    update

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -1,13 +1,20 @@
 <template>
-  <footer class="bg-[#AC1CE3] py-6 px-8 mt-4 tracking-wide">
+  <footer class="font-[Sarabun] bg-[#AC1CE3] py-6 px-8 mt-4 tracking-wide">
     <div class="relative max-w-screen-xl mx-auto">
       <div class="bg-[#ffffff] rounded-b-lg gap-3 rounded-md shadow-2xl flex flex-wrap items-center justify-between sm:px-8 max-sm:px-4 py-5 absolute top-[-66px] w-full">
-         <h1>footer</h1>
+         <h6 class="text-[#7c150d] sm:text-2xl max-sm:text-base font-bold">วิทยาลัยเทคนิคตราด</h6>
+         <button type="button" class="px-4 py-2 text-[15px] font-semibold text-white bg-[#802918] rounded-full gap-2 cursor-pointer">
+          สำนักงานคณะกรรมการ
+         </button>
       </div>
-     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 pt-15 px-4 rounded-b-md bg-[#AC1CE3]">
-
+     
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 pt-15 px-4 rounded-b-md bg-[#AC1CE3]">
+      อุอิอ๊า
      </div>
+ </div>
 
+    <div class="bg-[#AC1CE3] pt-5 sm:px-30 w-full ">
+      ตี่หน้าหู
     </div>
   </footer>
 
