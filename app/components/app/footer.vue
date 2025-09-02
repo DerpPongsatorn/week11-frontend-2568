7b4a49d342933b76b92f033e0c23b505ce02eaf7
Pongstorm Potita <gaymanxii1995@gmail.com>
2025-09-02 10:30:13 +0700

    update

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <h1>footer</h1>
-  </div>
+  <footer class="bg-[#AC1CE3] py-6 px-8 mt-4 tracking-wide">
+    <div class="relative max-w-screen-xl mx-auto">
+      <div class="bg-[#ffffff] rounded-b-lg gap-3 rounded-md shadow-2xl flex flex-wrap items-center justify-between sm:px-8 max-sm:px-4 py-5 absolute top-[-66px] w-full">
+         <h1>footer</h1>
+      </div>
+     
+    </div>
+  </footer>
+
 </template>
 
 <script lang="ts" setup>
@@ -9,5 +15,7 @@
 </script>
 
 <style>
+
+
 
 </style>
