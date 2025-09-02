02c03d64de46318bed131386e7b33e979442e983
Pongstorm Potita <gaymanxii1995@gmail.com>
2025-09-02 11:19:52 +0700

    update

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -10,20 +10,34 @@
      
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 pt-15 px-4 rounded-b-md bg-[#AC1CE3]">
       <div class="lg:col-span-1 max-w-md ">
-         <h4 class="text-base font-medium mb-3 text-gray-100 flex">
+        <h4 class="text-base font-medium mb-3 text-gray-100 flex">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
           <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
           <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
           </svg>
            วิทยาลัยเทคนิคตราด
-         </h4>
+        </h4>
+        <hr class="w-54 bg-gray-100 border-spacing-x-0.5 text-white rounded-sm dark:bg-gray-700" />
+        <ul class="space-y-2 ml-6 mc-2 text-white" style="list-style-type: square;">
+          <li class="text-white text-sm"><a href="#"></a>About Us</li>
+           <li class="text-white text-sm"><a href="#"></a>Contact Us</li>
+          
+        </ul>
       </div>
         
         </div>
  </div>
 
     <div class="bg-[#AC1CE3] pt-5 sm:px-30 w-full ">
-      ตี่หน้าหู
+      <div class="lg:col-span-1 max-w-md ">
+        <h4 class="text-base font-medium mb-3 text-gray-100 flex">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+          </svg>
+          สถิติการใช้งาน
+        </h4>
+        <hr class="w-54 bg-gray-100 border-spacing-x-0.5 text-white rounded-sm dark:bg-gray-700" />
+      </div>
     </div>
   </footer>
 
