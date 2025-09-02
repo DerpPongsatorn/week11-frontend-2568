1b1e0eec7866e894caeb9a9a968c3cca195cd8a7
Pongstorm Potita <gaymanxii1995@gmail.com>
2025-09-02 11:36:19 +0700

    update

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -1,14 +1,14 @@
 <template>
-  <footer class="font-[Sarabun] bg-[#802918] py-6 px-8 mt-4 tracking-wide">
+  <footer class="font-[Sarabun] bg-[#6b0892] py-6 px-8 mt-4 tracking-wide">
     <div class="relative max-w-screen-xl mx-auto">
       <div class="bg-[#ffffff] rounded-b-lg gap-3 rounded-md shadow-2xl flex flex-wrap items-center justify-between sm:px-8 max-sm:px-4 py-5 absolute top-[-66px] w-full"> 
         <h6 class="text-[#7c150d] sm:text-xl max-sm:text-base font-bold">วิทยาลัยเทคนิคตราด</h6>
-        <button type="button" class="px-4 py-2 text-[15px] font-semibold text-white bg-[#802918] rounded-full gap-2 cursor-pointer">
+        <button type="button" class="px-4 py-2 text-[15px] font-semibold text-white bg-[#6b0892] rounded-full gap-2 cursor-pointer">
           สํานักงานคณะกรรมการการอาชีวศึกษา
         </button>
       </div>
 
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 pt-15 px-4 rounded-b-md bg-[#802918]">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 pt-15 px-4 rounded-b-md bg-[#6b0892]">
         <div class="lg:col-span-1 max-w-md">
           <h4 class="text-base font-medium mb-3 text-gray-100 flex">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
@@ -137,7 +137,7 @@
       </div>
     </div>
 
-     <div class="bg-[#802918] pt-5 sm:px-30 w-full">
+     <div class="bg-[#6b0892] pt-5 sm:px-30 w-full">
       <hr class="flex-grow border-gray-300 my-4 mx-auto w-full" />
       <p class="text-white text-sm flex mx-auto justify-center">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 size-6">
